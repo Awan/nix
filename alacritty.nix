41b21026770ea77dab16f1582da2eac1e3756c48
Abdullah Khabir <abdullah@abdullah.today>
2022-08-03 13:56:44 +0500

    adding packages and other stuff in dedicated files
    splitting my setup to different files

--- alacritty.nix
+++ alacritty.nix
@@ -6,27 +6,28 @@
       enable = true;
       settings = {
         font = rec {
-          normal.family = "Mononoki";
-          bold.family = "Mononoki";
-          bold_italic.family = "Mononoki";
+          normal.family = "Meslo LG M";
+          regular.family = "Meslo LG M";
+          bold.family = "Meslo LG M";
+          bold_italic.family = "Meslo LG M";
           regular = { style = "Regular"; };
           italic = { style = "Italic"; };
           bold_italic = { style = "Bold Italic"; };
           bold = { style = "Bold"; };
-          size = 17;
+          size = 19;
         };
         offset = {
           x = 0;
           y = 0;
         };
-        window = { opacity = 0.8; };
+        window = { opacity = 0.9; };
         cursor = {
           style = "Underline";
           unfocused_hollow = false;
         };
         mouse = { hide_when_typing = true; };
         colors = {
-          cursor.cursor = "#ff0018";
+          #cursor.cursor = "#ff0018";
           text.cursor = "#bde077";
         };
       };
