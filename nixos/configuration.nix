9d4f10c04f8900986085913c0ce2d1ebc63e6e33
Abdullah Khabir <abdullah@abdullah.solutions>
2022-08-11 05:20:21 +0000

    adding minimal configuration for boot

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -48,9 +48,8 @@
 
 # LUKS
 # Add UUID of your encrypted device
-    boot.initrd.luks.devices.luksroot =
-    {
-        device = "/dev/disk/by-uuid/a85807c5-361b-47df-9d86-5639d1902615";
+    boot.initrd.luks.devices.luksroot =     {
+        device = "/dev/disk/by-uuid/168fac67-b88c-4c22-8828-eba8ac71a2b3";
         allowDiscards = true;
         preLVM = true;
     };
